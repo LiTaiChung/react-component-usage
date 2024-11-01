ed5a2b72ecaab234dd81e7f2d934dd07f76d1d41
Ice Li <ice.jocard@gmail.com>
2024-11-02 01:20:42 +0800

    feat: use yarn 4.5.1 & install storybook

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@/components/ui/button';
+import { Button } from '@/elements/button';
 import {
   Card,
   CardContent,
@@ -6,7 +6,7 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from '@/components/ui/card';
+} from '@/elements/card';
 
 const Dashboard = () => {
   return (
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@/components/ui/button';
+import { Button } from '@/elements/button';
 
 const Home = () => {
   return (
